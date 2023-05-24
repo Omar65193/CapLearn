cccc541bd3f623e00e5514c9058f57e74b8853a3
AuroraPerez <auro.prz1809@gmail.com>
2023-05-23 18:51:34 -0600

    Add location plugin

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -7,6 +7,18 @@ import { ActionSheet, ActionSheetButtonStyle } from '@capacitor/action-sheet';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import { Toast } from '@capacitor/toast';
+import { Geolocation } from '@capacitor/geolocation';
+
+function showToast(message: string){
+  const showToast = async () => {
+    await Toast.show({
+      text: message,
+      duration: 'short',
+      position: 'center'
+    });
+  };
+  showToast();
+}
 
 const Tab1: React.FC = () => {  
   const texto = 'Un Action Sheet en Ionic es un componente de interfaz de usuario que muestra una lista de acciones o opciones al usuario en forma de hoja deslizable desde la parte inferior de la pantalla. Es una forma común de presentar un conjunto de opciones o acciones contextualmente relevantes para que el usuario las seleccione.';
@@ -18,8 +30,20 @@ const Tab1: React.FC = () => {
 
   const closeActionSheet = () => {
     setShowActionSheet(false);
+  };   
+  var latitud = "No se obtuvo la latitud";
+  var longitude = "No se obtuvo la longitud";
+  var accuracy = "No se obtuvo la certeza";
+  const printCurrentPosition = async () => {
+    const res = await Geolocation.getCurrentPosition();
+    if(res.coords.latitude!=undefined)
+      latitud = "Latitud: "+res.coords.latitude.toString();
+    if(res.coords.latitude!=undefined)
+      longitude = "Longitud: "+res.coords.longitude.toString();
+    if(res.coords.accuracy!=undefined)
+      accuracy = "Certeza: "+res.coords.accuracy.toString();      
   };
-    
+  printCurrentPosition();
     return (
       <IonPage>
         <IonHeader>
@@ -36,45 +60,24 @@ const Tab1: React.FC = () => {
               onDidDismiss={closeActionSheet}
               buttons={[
                 {
-                  text: 'Mensaje 1',
+                  text: 'Mostrar mi latitud',
                   handler: () => {                   
                     closeActionSheet();
-                    const showToast = async () => {
-                      await Toast.show({
-                        text: 'Este es un Toast',
-                        duration: 'short',
-                        position: 'center'
-                      });
-                    };
-                    showToast();
+                    showToast(latitud);
                   }
                 },
                 {
-                  text: 'Mensaje 2',
+                  text: 'Mostrar mi longitud',
                   handler: () => {                    
                     closeActionSheet();
-                    const showToast = async () => {
-                      await Toast.show({
-                        text: 'Este es un plugin',
-                        duration: 'short',
-                        position: 'center'
-                      });
-                    };
-                    showToast();
+                    showToast(longitude);
                   }
                 },
                 {
-                  text: 'Mensaje 3',
+                  text: 'Mostrar la certeza',
                   handler: () => {
                     closeActionSheet();
-                    const showToast = async () => {
-                      await Toast.show({
-                        text: 'Este muestra un texto',
-                        duration: 'short',
-                        position: 'center'
-                      });
-                    };
-                    showToast();
+                    showToast(accuracy);
                   }
                 }
               ]}
